08551751b26caac324be95e53ecf9747f8631841
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-11-13 14:42:58 +0000

    con BTreePrinter

--- arbolBinarioBusqueda/ArbolBinarioBusqueda.java
+++ arbolBinarioBusqueda/ArbolBinarioBusqueda.java
@@ -17,13 +17,44 @@ public class ArbolBinarioBusqueda extends ArbolBinario{
         nuevo.setValor(dato);
         if (raiz == null)
             raiz = nuevo;
-        else
-            {
-
+        else{       
+            //anterior: referencia al padre aux
+            Nodo anterior = null;
+            //aux: auxiliar qur va recorriendo los nodos, dese la raiz
+            Nodo aux=raiz;
+            while (aux != null){
+                anterior = aux;
+                if (dato.esMenor(aux.getValor()))
+                    aux = aux.getIzquierdo();
+                else if (dato.esMayor(aux.getValor()))
+                    aux = aux.getDerecho();
+                else 
+                    throw new Exception("Dato duplicado");
+                
             }
-            ///aqui me quede
-        
-
-        
+            if(dato.esMenor(anterior.getValor()))
+                anterior.setIzquierdo(nuevo);
+            else
+                anterior.setDerecho(nuevo);
+        }
+       
+    }
+    public Nodo buscar(Object valor){
+        Comparable dato = (Comparable)valor;
+        if (raiz==null){
+            return null;
+        }else{
+            //aux: auxiliar que va recorriendo los nodos, desde la reiz
+            Nodo aux = raiz;
+            while (aux !=null){
+                if(dato.esMenor(aux.getValor()))
+                   aux = aux.getIzquierdo();
+                else if (dato.esMayor(aux.getValor()))
+                   aux = aux.getDerecho();
+                else
+                    return aux;
+            }
+            return null;
+        }
     }
 }
